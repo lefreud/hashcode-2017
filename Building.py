beec04e6d947241c6368bf4cc40968c5da51fd5b
Antojeux <antonyma2222@gmail.com>
2021-10-25 16:33:11 -0400

    diminuer grid

--- Building.py
+++ Building.py
@@ -11,12 +11,16 @@ class Building:
         self.budget = data['budget']
         self.backbone_pos = data['backbone_pos']
         self.targets = []
+        self.walls = []
+        self.create_liste_targets_and_mur()
 
-    def create_liste_targets(self):
+    def create_liste_targets_and_mur(self):
         for i in range(len(self.grid)):
             for j in range(len(self.grid[i])):
                 if self.grid[i][j] == ".":
                     self.targets.append((i,j))
+                elif self.grid[i][j] == "#":
+                    self.walls.append((i,j))
 
     def calculer_score(self, liste_routers: list[tuple], liste_backbones: list[tuple]) -> int:
         return 1000*len(self.targets) + self.budget - len(liste_backbones)*self.cout_backbones - len(liste_routers)*self.cout_routers
@@ -34,7 +38,7 @@ class Building:
             liste_connection_temp.extends(connections_temp)
             liste_routeur_temp = liste_routeur
             liste_routeur_temp.append(target)
-            targets_diminuer = self.diminuer_grid(target)
+            targets_diminuer = self.diminuer_grid(target, targets)
             score_temp, liste_routeur, liste_connection = self.algo(targets_diminuer, liste_routeur_temp, liste_connection_temp)
             if score_temp < score_max:
                 score_max = score_temp
@@ -46,5 +50,17 @@ class Building:
     def calculer_liaison_routeur(self, target, liste_connection):
         #TODO
 
-    def diminuer_grid(self, new_target):
-        #TODO
+    def diminuer_grid(self, new_target, targets):
+        targets_temp = targets
+        for target in targets_temp:
+            if abs(new_target[0] - target[0]) <= self.router_range and abs(new_target[1] - target[1]) <= self.router_range:
+                if self.check_if_wall(target, new_target):
+                    targets_temp.remove(target)
+        return targets_temp
+
+
+    def check_if_wall(self, target, router_pos):
+        for wall in self.walls:
+            if min(router_pos[0],target[0]) <= wall[0] <= max(router_pos[0],target[0]) and min(router_pos[1],target[1]) <= wall[1] <= max(router_pos[1],target[1]):
+                return True
+        return False
