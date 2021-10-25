33062600faaa009fcf25879acece2a1e795dbea5
JLague <justin.lague@hotmail.com>
2021-10-25 15:18:12 -0400

    remove Util class

--- Util.py
+++ Util.py
@@ -12,33 +12,29 @@ class Situation(Enum):
     OPERA = f"{_root}/opera.in"
     LONDRES = f"{_root}/rue_de_londres.in"
 
-class Util:
-    def __init__(self):
-        pass
+def parse_in(input: Situation) -> dict:
+    with open(input.value) as file:
+        # Read numbers
+        h, w, r = map(int, file.readline().split())
+        pb, pr, b = map(int, file.readline().split())
+        bx, by = map(int, file.readline().split())
 
-    def parse_in(input: Situation) -> dict:
-        with open(input.value) as file:
-            # Read numbers
-            h, w, r = map(int, file.readline().split())
-            pb, pr, b = map(int, file.readline().split())
-            bx, by = map(int, file.readline().split())
+        # Read grid
+        grid = []
+        for _ in range(h):
+            grid.append([char for char in file.readline()[:-1]])
 
-            # Read grid
-            grid = []
-            for _ in range(h):
-                grid.append([char for char in file.readline()[:-1]])
+        np_array = np.array(grid)
+    
+    return {"dim": (h, w), "radius": r, "backbone_price": pb, "router_price": pr,
+            "budget": b, "backbone_pos": (bx, by), "grid": np_array}
 
-            np_array = np.array(grid)
-        
-        return {"dim": (h, w), "radius": r, "backbone_price": pb, "router_price": pr,
-                "budget": b, "backbone_pos": (bx, by), "grid": np_array}
-
-    def parse_out():
-        pass
+def parse_out(backbone_cells: list[tuple], router_cells: list[tuple]):
+    pass
 
 
 def main():
-    out = Util.parse_in(Situation.CHARLESTON)
+    out = parse_in(Situation.CHARLESTON)
     print(out["grid"])
 
 if __name__ == "__main__":
